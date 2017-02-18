66ab57d7904554b0eb2f493b3d0dc1e686a66dac
Rafael Ríos Moya <rafael@fintonic.com>
2017-02-18 16:52:16 +0100

    delete root access

--- src/main/java/es/org/ganchix/SlackCommandLaunchApplication.java
+++ src/main/java/es/org/ganchix/SlackCommandLaunchApplication.java
@@ -3,7 +3,6 @@ package es.org.ganchix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
@@ -11,11 +10,6 @@ public class SlackCommandLaunchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SlackCommandLaunchApplication.class, args);
-    }
-
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello world";
     }
 
 }
