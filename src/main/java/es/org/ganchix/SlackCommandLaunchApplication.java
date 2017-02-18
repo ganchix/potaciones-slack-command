075160f3f95b63d2765a454f5745a3a42a969e38
Rafael Ríos Moya <rafael@fintonic.com>
2017-02-18 16:45:02 +0100

    Added root mapping

--- src/main/java/es/org/ganchix/SlackCommandLaunchApplication.java
+++ src/main/java/es/org/ganchix/SlackCommandLaunchApplication.java
@@ -3,6 +3,7 @@ package es.org.ganchix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
@@ -10,6 +11,11 @@ public class SlackCommandLaunchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SlackCommandLaunchApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello world";
     }
 
 }
